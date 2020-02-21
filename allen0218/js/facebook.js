9d8c8c6fee8cc373ccd0bdfb058b0545e37b3c19
Allen <a0935890130@gmail.com>
2020-02-21 13:23:21 +0800

    0218作業

---
+++ allen0218/js/facebook.js
@@ -0,0 +1,9 @@
+function opensidebar() {
+    document.getElementById("sidebarsmall").style.display = "none";
+    document.getElementById("sidebar").style.display = "block";
+}
+
+function closesidebar() {
+    document.getElementById("sidebar").style.display = "none";
+    document.getElementById("sidebarsmall").style.display = "block";
+}
